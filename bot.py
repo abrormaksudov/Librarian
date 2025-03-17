e116f21b772ec2cc6f1de994e6fda9a6d67e6f9c
abrormaksudov <93431820+abrormaksudov@users.noreply.github.com>
2025-03-17 23:30:23 +0500

    tiny database add

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import os
+import shelve
 from typing import Any
 
 import fitz
@@ -9,11 +10,42 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.client.telegram import TelegramAPIServer
 from aiogram.enums import ParseMode
+from aiogram.filters import Command
 from aiogram.types import FSInputFile, InputMediaDocument
 from config_reader import config
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 router = Router()
+BOOK_SHELF = "book.shelf"
+
+# def get_file_hash(file_path, chunk_size=8192):
+#     hash_sha256 = hashlib.sha256()
+#     with open(file_path, 'rb') as f:
+#         for chunk in iter(lambda: f.read(chunk_size), b''):
+#             hash_sha256.update(chunk)
+#     return hash_sha256.hexdigest()
+
+def update_file_data(file_unique_id, pages):
+    with shelve.open(BOOK_SHELF, flag='c') as db:
+        db[file_unique_id] = pages
+    print(f"Updated data for key: {file_unique_id}")
+
+def load_data():
+    with shelve.open(BOOK_SHELF, flag='r') as db:
+        return dict(db)
+
+def remove_file_data(file_unique_id):
+    with shelve.open(BOOK_SHELF, flag='w') as db:
+        if file_unique_id in db:
+            del db[file_unique_id]
+            print(f"Removed data for key: {file_unique_id}")
+
+@router.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+    print(message.message_thread_id)
+    print(message.chat.id)
 
 @router.message(F.document, F.message_thread_id, ~F.message_thread_id.in_([738, 741]))
 async def process_document(message: types.Message, bot: Bot) -> Any:
@@ -27,12 +59,14 @@ async def process_document(message: types.Message, bot: Bot) -> Any:
     full_title = metadata.get('title')
     authors, title = map(str.strip, full_title.split(":", 1))
     npage = document_file.page_count
+    document_file.close()
     caption = (f"<b>Title:</b> <code>{title}</code>\n"
                f"<b>Authors:</b> <code>{authors}</code>\n"
                f"<b>Pages:</b> <code>{npage}</code>. <b>Format:</b> <code>{ext}</code>.")
     document = FSInputFile(path=file_path, filename=title + "." + ext)
 
     await message.delete()
+    update_file_data(message.document.file_unique_id, npage)
     if message.reply_to_message.forum_topic_created:
         await message.answer_document(
             document=document,
@@ -44,6 +78,7 @@ async def process_document(message: types.Message, bot: Bot) -> Any:
                           f"Previously, it was:\n\n"
                           f"{message.reply_to_message.caption}")
 
+        remove_file_data(message.reply_to_message.document.file_unique_id)
         await message.bot.send_document(
             chat_id=message.from_user.id,
             document=message.reply_to_message.document.file_id,
@@ -54,6 +89,17 @@ async def process_document(message: types.Message, bot: Bot) -> Any:
             message_id=message.reply_to_message.message_id,
             media=InputMediaDocument(media=document, caption=caption)
         )
+
+    book_shelve = load_data()
+    total_books = len(book_shelve)
+    total_pages = sum(book_shelve.values())
+    stats_text = (f"Total books: {total_books}\n"
+                  f"Total pages: {total_pages}")
+    await bot.edit_message_text(
+        chat_id=message.chat.id,
+        message_id=943,
+        text=stats_text
+    )
 
     os.remove(file_path)
 
