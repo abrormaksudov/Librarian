9f93b052a29972919577b066218ee88fe41266b4
abrormaksudov <93431820+abrormaksudov@users.noreply.github.com>
2025-03-17 21:54:47 +0500

    implement local telegram bot api

--- bot.py
+++ bot.py
@@ -1,50 +1,71 @@
 import asyncio
 import logging
+import os
 from typing import Any
 
+import fitz
 from aiogram import Bot, Dispatcher, Router, F, types
-from aiogram.filters.command import Command
-from aiogram.types import InputMediaDocument
+from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.client.telegram import TelegramAPIServer
+from aiogram.enums import ParseMode
+from aiogram.types import FSInputFile, InputMediaDocument
 from config_reader import config
 
 logging.basicConfig(level=logging.INFO)
-
 router = Router()
 
-@router.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+@router.message(F.document, F.message_thread_id, ~F.message_thread_id.in_([738, 741]))
+async def process_document(message: types.Message, bot: Bot) -> Any:
+    file_id = message.document.file_id
+    file = await bot.get_file(file_id, request_timeout=300)
+    file_path = file.file_path
+    document_file = fitz.open(file_path)
+    metadata = document_file.metadata
 
-@router.message(F.document)
-async def cmd_doc(message: types.Message) -> Any:
-    caption = message.document.file_name.replace("_", " ")
+    ext = message.document.file_name.rsplit(".", 1)[-1]
+    full_title = metadata.get('title')
+    authors, title = map(str.strip, full_title.split(":", 1))
+    npage = document_file.page_count
+    caption = (f"<b>Title:</b> <code>{title}</code>\n"
+               f"<b>Authors:</b> <code>{authors}</code>\n"
+               f"<b>Pages:</b> <code>{npage}</code>. <b>Format:</b> <code>{ext}</code>.")
+    document = FSInputFile(path=file_path, filename=title + "." + ext)
+
     await message.delete()
     if message.reply_to_message.forum_topic_created:
         await message.answer_document(
-            message.document.file_id,
+            document=document,
             caption=caption
         )
-    else:
+    elif message.reply_to_message.from_user.is_bot:
+        link = message.reply_to_message.get_url(include_thread_id=True)
+        text_to_sender = (f"The <a href='{link}'>book</a> has been modified.\n"
+                          f"Previously, it was:\n\n"
+                          f"{message.reply_to_message.caption}")
+
+        await message.bot.send_document(
+            chat_id=message.from_user.id,
+            document=message.reply_to_message.document.file_id,
+            caption=text_to_sender
+        )
         await message.bot.edit_message_media(
             chat_id=message.chat.id,
             message_id=message.reply_to_message.message_id,
-            media=InputMediaDocument(media=message.document.file_id,
-                                     caption=caption)
+            media=InputMediaDocument(media=document, caption=caption)
         )
 
-@router.edited_message()
-async def edited_message_handler(edited_message: types.Message) -> Any:
-    await edited_message.answer("You edited it!")
-
-@router.message(F.chat.type.in_(["group", "supergroup"]))
-async def cmd_sup(message: types.Message):
-    if message.reply_to_message.forum_topic_created:
-        await message.answer("Sup!")
-    else:
-        await message.reply_to_message.edit_text("I changed it!")
+    os.remove(file_path)
 
 async def main():
-    bot = Bot(token=config.bot_token.get_secret_value())
+    local_server = TelegramAPIServer.from_base('http://localhost:8081')
+    session = AiohttpSession(api=local_server)
+
+    bot = Bot(
+        token=config.bot_token.get_secret_value(),
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+        session=session
+    )
     dp = Dispatcher()
     dp.include_routers(router)
 
